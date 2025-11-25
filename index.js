a82ec353ba2b1f09d995152030ee3876d797c0e8
abdimalikworrier <abdimalikadan13@gmail.com>
2025-11-25 09:56:28 +0300

    done

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 
 // Write your code here
+let num1 = 31
+let num2 = 2
+let multiply=num1*num2
+let random = Math.floor(Math.random() * 100) + 1;
+let num3= 18
+let num4= 7
+let mod= num3%num4
+let number=(1,10,20)
+let max=Math.max(1,10,20)
+
+
+
